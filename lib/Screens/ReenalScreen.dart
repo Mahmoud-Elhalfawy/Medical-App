d9918faa96868535b5bb6fab589c0c866497bf96
mahmoud_elhalfawy <dodostrike199990>
2021-04-16 16:16:43 +0200

    set state error resolved and added new parameter in create field function

--- lib/Screens/ReenalScreen.dart
+++ lib/Screens/ReenalScreen.dart
@@ -11,7 +11,7 @@ class ReenalScreen extends StatefulWidget {
 }
 
 class _ReenalScreenState extends State<ReenalScreen>
-    with SingleTickerProviderStateMixin{
+    with SingleTickerProviderStateMixin {
   AnimationController _animationController;
   final weightController = TextEditingController();
   final PNAController = TextEditingController();
@@ -24,14 +24,13 @@ class _ReenalScreenState extends State<ReenalScreen>
   bool enableHoursText = true;
   bool createCalendarField = false;
   img.Image photo;
-  bool flag = false;
+  bool isWeeks = false;
   String choice;
-
 
   //-------------------New Code added
   //--------------------------
-  Widget toggleChoice(bool flag){
-    if(flag){
+  Widget toggleChoice(bool flag) {
+    if (flag) {
       return FadeTransition(
         opacity: _animationController,
         child: Column(children: <Widget>[
@@ -44,7 +43,7 @@ class _ReenalScreenState extends State<ReenalScreen>
           ),
         ]),
       );
-    }else{
+    } else {
       return FadeTransition(
         opacity: _animationController,
         child: Text(
@@ -58,85 +57,18 @@ class _ReenalScreenState extends State<ReenalScreen>
     }
   }
 
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1700),
+    );
+    _animationController.forward();
+  }
 
-
-  //-----------------------------
-  // Widget createCalendar(bool createCalendarField) {
-  //   if (createCalendarField) {
-  //     return FadeTransition(
-  //       opacity: _animationController,
-  //       child: Column(children: <Widget>[
-  //         DateTimePicker(
-  //           type: DateTimePickerType.dateTimeSeparate,
-  //           dateMask: 'd MMM, yyyy',
-  //           initialValue: DateTime.now().toString(),
-  //           firstDate: DateTime(2000),
-  //           lastDate: DateTime(2100),
-  //           icon: Icon(Icons.event),
-  //           dateLabelText: 'Date',
-  //           timeLabelText: "Hour",
-  //           validator: (val) {
-  //             dateAndTime = val;
-  //             return dateAndTime;
-  //           },
-  //           onChanged: (val) => dateAndTime = val,
-  //           onSaved: (val) => dateAndTime = val,
-  //         ),
-  //         SizedBox(
-  //           height: 10,
-  //         ),
-  //         Text(
-  //           '(enter birth date and time)',
-  //           style: TextStyle(
-  //             color: Colors.black26,
-  //             fontSize: 15,
-  //           ),
-  //         ),
-  //       ]),
-  //     );
-  //   } else {
-  //     return FadeTransition(
-  //       opacity: _animationController,
-  //       child: Text(
-  //         '(enter age in hours)',
-  //         style: TextStyle(
-  //           color: Colors.black26,
-  //           fontSize: 15,
-  //         ),
-  //       ),
-  //     );
-  //   }
-  // }
-
-  // Future<bool> getResults() async {
-  //   if (weightController.text.isEmpty || PNAController.text.isEmpty)
-  //     return false;
-  //
-  //   App1Controller app1Controller = App1Controller(context);
-  //   double weight = double.parse(weightController.text.toString());
-  //   double serum = double.parse(PNAController.text.toString());
-  //   app1Controller.calculateResult(
-  //       enableHoursText, hemolytic, weightController, dateAndTime, weight, serum);
-  //
-  //   return true;
-  // }
-
-
-  //---------------------
- void initState(){
-   super.initState();
-   _animationController = AnimationController(
-     vsync: this,
-     duration: Duration(milliseconds: 1700),
-   );
-   _animationController.forward();
- }
-
-
-
- //create Field
+  //create Field
   Widget createField(IconData icon, TextEditingController controller,
-      String text, bool enabled) {
+      String text, bool enabled, String image) {
     return TextField(
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       enabled: enabled,
@@ -155,17 +87,14 @@ class _ReenalScreenState extends State<ReenalScreen>
         focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(25),
             borderSide: BorderSide(color: Colors.green[700])),
-        prefixIcon: Icon(icon),
+        prefixIcon: image==null?Icon(icon):ImageIcon(AssetImage(image)),
         hintText: text,
       ),
     );
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -177,8 +106,7 @@ class _ReenalScreenState extends State<ReenalScreen>
             ),
           ),
           child: Center(
-            child:
-            CustomScrollView(
+            child: CustomScrollView(
               slivers: <Widget>[
                 SliverAppBar(
                   toolbarHeight: 60,
@@ -199,31 +127,27 @@ class _ReenalScreenState extends State<ReenalScreen>
                   ),
                   leading: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Container(child: Image.asset('images/bilirubin_logo.png')),
+                    child: Container(
+                        child: Image.asset('images/bilirubin_logo.png')),
                   ),
                 ),
-
                 SliverToBoxAdapter(
                   child: IntrinsicHeight(
                     child: Container(
                       constraints: BoxConstraints(
-                        minHeight: MediaQuery.of(context).size.height*0.9,
+                        minHeight: MediaQuery.of(context).size.height * 0.9,
                         //maximum height set to 100% of vertical height
-
-
                       ),
                       child: Column(
                           crossAxisAlignment: CrossAxisAlignment.stretch,
                           mainAxisAlignment: MainAxisAlignment.center,
                           mainAxisSize: MainAxisSize.max,
                           children: <Widget>[
-
                             Expanded(
                               child: Container(
-
-
                                 padding: EdgeInsets.all(15),
-                                margin: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+                                margin: EdgeInsets.symmetric(
+                                    vertical: 15, horizontal: 15),
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(25),
                                   color: Colors.white60,
@@ -231,28 +155,30 @@ class _ReenalScreenState extends State<ReenalScreen>
                                 clipBehavior: Clip.antiAlias,
                                 child: Column(
                                   mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
                                   children: <Widget>[
-                                    createField(Icons.accessibility,
-                                        weightController, 'Weight (Kg)', true,),
+                                    createField(
+                                      Icons.accessibility,
+                                      weightController,
+                                      'Weight (Kg)',
+                                      true,null
+                                    ),
                                     SizedBox(
                                       height: 18,
                                     ),
                                     Row(
                                       mainAxisSize: MainAxisSize.max,
                                       mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
+                                          MainAxisAlignment.spaceAround,
                                       children: <Widget>[
                                         Expanded(
-
-
+                                          
                                           child: createField(
                                               Icons.hourglass_full_sharp,
                                               PNAController,
-                                              'Enter PNA (Weeks/Days)',
-                                              true),
-
-
+                                              'Enter PNA',
+                                              true,"images/PNA.png"),
                                         ),
                                         SizedBox(
                                           width: 30,
@@ -264,53 +190,54 @@ class _ReenalScreenState extends State<ReenalScreen>
                                             textOn: 'Weeks',
                                             colorOff: Colors.blueAccent,
                                             colorOn: Colors.green,
-                                            iconOff: Icons.calendar_today_outlined,
-                                            iconOn: Icons.calendar_today_outlined,
+                                            iconOff:
+                                                Icons.calendar_today_outlined,
+                                            iconOn:
+                                                Icons.calendar_today_outlined,
                                             onChanged: (bool position) {
-                                              if (position) {
-                                                setState(() {
-                                                  flag = true;
-                                                  // enableHoursText = false;
-                                                  // createCalendarField = true;
-                                                });
-                                              }else {
-                                                flag = false;
-                                                // enableHoursText = true;
-                                                // createCalendarField = false;
-                                              }
+                                              if(position!=isWeeks)
+                                              setState(() {
+                                                   isWeeks=position;
+                                              });
+
                                             },
                                           ),
                                         ),
                                       ],
                                     ),
-
                                     SizedBox(
                                       height: 30,
                                     ),
-                                    toggleChoice(flag),
+                                    toggleChoice(isWeeks),
                                     SizedBox(
                                       height: 20,
-
                                     ),
-                                    createField(Icons.ac_unit, GAController, 'Enter G.A (Weeks)', true),
+                                    createField(Icons.ac_unit, GAController,
+                                        'Enter G.A (Weeks)', true,null),
                                     SizedBox(
                                       height: 18,
                                     ),
-
                                     SizedBox(
                                       height: 10,
                                     ),
-                                    createField(Icons.height, heightController, 'Enter Height (Cm)', true),
+                                    createField(Icons.height, heightController,
+                                        'Enter Height (Cm)', true,null),
                                     SizedBox(
                                       height: 18,
                                     ),
-                                    createField(Icons.source_outlined,SCRController,
-                                        "Enter SCR (mg/dl)", true),
+                                    createField(
+                                        Icons.source_outlined,
+                                        SCRController,
+                                        "Enter SCR (mg/dl)",
+                                        true,"images/SCR.png"),
                                     SizedBox(
                                       height: 18,
                                     ),
-                                    createField(Icons.access_alarm_rounded, ureaController,
-                                        'Enter Urea Rate (mh/dc)', true),
+                                    createField(
+                                        Icons.access_alarm_rounded,
+                                        ureaController,
+                                        'Enter Urea Rate (mh/dc)',
+                                        true,null),
                                     SizedBox(
                                       height: 18,
                                     ),
@@ -328,20 +255,17 @@ class _ReenalScreenState extends State<ReenalScreen>
                                         ),
                                         shape: new RoundedRectangleBorder(
                                           borderRadius:
-                                          new BorderRadius.circular(30.0),
+                                              new BorderRadius.circular(30.0),
                                         ),
                                         color: Colors.green[600],
-                                        onPressed: () async {
-
-                                        },
+                                        onPressed: () async {},
                                       ),
                                     ),
                                   ],
                                 ),
                               ),
                             ),
-                          ]
-                      ),
+                          ]),
                     ),
                   ),
                 ),
@@ -351,13 +275,5 @@ class _ReenalScreenState extends State<ReenalScreen>
         ),
       ),
     );
-   
-      
   }
-
-
-
-
-
-
 }
